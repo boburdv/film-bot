fbe3f83f95ea879d554be20933d253bafe734a3b
boburdv <boburdv@gmail.com>
2025-09-30 05:08:38 -0700

    fix-buyurtma

--- handlers/buyurtma.js
+++ handlers/buyurtma.js
@@ -8,9 +8,9 @@ module.exports = async function handleBuyurtma(bot, ctx) {
   const userId = ctx.from.id;
   waitOrder[userId] = true;
 
-  const msg = await ctx.telegram.copyMessage(userId, CHANNEL, 54, {
-    caption: "𝗤𝗼'𝗹𝗹𝗮𝗻𝗺𝗮 𝘃𝗶𝗱𝗲𝗼𝘀𝗶❕\n\nTelegram kanal: @movely_studios \n\nYangi film nomini yozib qoldirishingiz mumkin!",
-    reply_markup: Markup.inlineKeyboard([[Markup.button.callback("🔙 Ortga qaytish", "go_back")]]).reply_markup,
+  const msg = await ctx.telegram.copyMessage(userId, CHANNEL, 75, {
+    caption: "Buyurtma qilishdan oldin ushbu videoni albatta ko'ring!\n\nTelegram kanal: @movely_studios",
+    reply_markup: Markup.inlineKeyboard([[Markup.button.callback("Ortga qaytish", "go_back")]]).reply_markup,
   });
 
   userLast[userId] = { specialMsg: msg.message_id };
